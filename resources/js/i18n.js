672252e41ae2524618ab45b08f8cef167bb8e42d
juanleonel <juanl.albores.c@gmail.com>
2026-07-09 00:00:46 -0500

    added internationalization

---
+++ resources/js/i18n.js
@@ -0,0 +1,71 @@
+import { AppConstants } from './constants/app.constants.js';
+
+let translations = {};
+let currentLang = AppConstants.currentLang;
+
+export async function initI18n() {
+  try {
+    const locale = await Neutralino.os.getEnv('LANG') || 'es_ES.UTF-8';
+    currentLang = locale.startsWith(AppConstants.language.en) ?
+      AppConstants.language.en : AppConstants.currentLang;
+  } catch {
+    currentLang = AppConstants.currentLang;
+  }
+
+  console.log('currentLang ', currentLang)
+
+  await loadLanguage(currentLang);
+
+}
+
+async function loadLanguage(lang) {
+    try {
+      const response = await fetch(`js/i18n/${lang}.json`);
+      translations = await response.json();
+      currentLang = lang;
+      await Neutralino.storage.setData('preferredLang', lang);
+      applyTranslations();
+    } catch (error) {
+      console.error("Error to load lang:", JSON.stringify(error));
+    }
+}
+
+function applyTranslations() {
+  document.querySelectorAll('[data-i18n]').forEach(el => {
+    const key = el.getAttribute('data-i18n');
+
+    if (translations[key]) {
+       console.log(el.tagName)
+      if (el.tagName === 'INPUT' || el.tagName === 'TEXTAREA' || el.tagName === 'BUTTON') {
+        el.placeholder = translations[key];
+      } else {
+        el.innerText = translations[key];
+      }
+    }
+  });
+
+  if (translations['window.title']) {
+    document.title = translations['window.title'];
+  }
+
+  document.querySelectorAll('button').forEach(btn => {
+    checkButton(btn);
+  });
+}
+
+function checkButton(tag) {
+  if (!tag || tag.tagName !== 'BUTTON') return;
+
+  if (tag.classList.contains('delete-btn')) {
+    tag.textContent = translations['btn.delete'] || 'Eliminar';
+  }
+}
+
+export async function toggleLanguage() {
+    const newLang = currentLang === AppConstants.currentLang ? AppConstants.language.en : AppConstants.currentLang;
+    await loadLanguage(newLang);
+}
+
+export function translated(key) {
+  return translations[key] || key;
+}
